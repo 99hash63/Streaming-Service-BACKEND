27b9841dd626dcaa9bd3b65e438261c5a2d6eeb0
99hash63 <thkottegoda@gmail.com>
2021-07-15 15:06:17 +0530

    Initial commit with folder structure

---
+++ backend/src/index.js
@@ -0,0 +1,52 @@
+const config = require('config')
+const express = require('express');
+const mongoose = require('mongoose');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const dotenv = require ('dotenv');
+const app = express();
+// const cookieParser = require("cookie-parser");
+require("dotenv").config();
+
+const PORT = process.env.PORT || config.get('server.port')
+const host = config.get('server.host')
+
+app.use(cors({
+    origin: [host],
+    credentials: true,
+}));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+    res.send("It works"); 
+});
+
+app.use(express.json());
+// app.use(cookieParser());
+ 
+//connect to mongoDB
+const URL= process.env.MONGODB_URL;
+
+mongoose.connect(URL,{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useUnifiedTopology:true,
+    useFindAndModify:false
+})
+const connection =mongoose.connection;
+connection.once("open", ()=>{
+    console.log("MongoDB connection success")
+})
+
+
+app.listen(PORT,()=>{
+    console.log(`Server running on PORT: ${PORT}`)
+})
+
+
+
+
+
+
+
+
