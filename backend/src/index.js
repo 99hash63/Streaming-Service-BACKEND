3ff816ad8a3c3f5b9f4004124e255a4eb4d15a09
99hash63 <thkottegoda@gmail.com>
2021-07-16 08:13:42 +0530

    a simple code for streaming service has been created

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,7 +7,12 @@ const dotenv = require ('dotenv');
 const app = express();
 const cookieParser = require("cookie-parser");
 //filse system import for video stream
-const fs = require("fs");
+// const fs = require("fs");
+// const multer = require("multer");
+// const GridFsStorage = require("multer-gridfs-storage");
+// const Grid = require("gridfs-stream");
+// const methodOverride = require("method-override")
+
 require("dotenv").config();
 
 const PORT = process.env.PORT || config.get('server.port')
@@ -27,6 +32,8 @@ app.get("/", (req, res) => {
 app.use(express.json());
 //middleware to pass cookie into req.cookies
 app.use(cookieParser());
+
+// app.user(methodOverride('_method'));
  
 //connect to mongoDB
 const URL= process.env.MONGODB_URL;
@@ -40,7 +47,13 @@ mongoose.connect(URL,{
 const connection =mongoose.connection;
 connection.once("open", ()=>{
     console.log("MongoDB connection success")
+    //Initialize stream
+    // gfs = Grid(conn.db, mongoose.mongo);
+    // gfs.collection('uploads');
 })
+
+//create storage engine
+
 
 
 app.listen(PORT,()=>{
@@ -52,33 +65,43 @@ app.listen(PORT,()=>{
 app.use("/auth", require("./api/routers/userRouter"))
 app.use("/customer", require("./api/routers/customerRouter"))
 
-//get video endpoint
-app.get('/video', (req, res) => {
-  const range = req.headers.range;
-  if(!range){
-      res.status(400).send("Requires Ramge header")
-  }
-  const videoPath = "../myVid.mp4";
-  const videoSize = fs.statSync("myVid.mp4").size;
+// //get video endpoint
+// app.get('/video', (req, res) => {
+//   const range = req.headers.range;
+//   if(!range){
+//       res.status(400).send("Requires Ramge header")
+//   }
+//   const videoPath = "../myVid.mp4";
+//   const videoSize = fs.statSync("myVid.mp4").size;
 
-  // Parse Range
-  const CHUNK_SIZE = 10 ** 6; //1MB
-  const START = Number(range.replace(/\D/g, ""));
-  const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
+//   // Parse Range
+//   const CHUNK_SIZE = 10 ** 6; //1MB
+//   const START = Number(range.replace(/\D/g, ""));
+//   const end = Math.min(start + CHUNK_SIZE, videoSize - 1);
 
-  const contentLength = end - start + 1;
-  const headers = {
-      "Content-Range": `bytes ${start}-${end}/${videoSize}`,
-      "Accept-Ranges": "bytes",
-      "Content-Length": "contentLength",
-      "Content-Type": "video/mp4",
-  };
-  res.writeHead(206, headers);
+//   const contentLength = end - start + 1;
+//   const headers = {
+//       "Content-Range": `bytes ${start}-${end}/${videoSize}`,
+//       "Accept-Ranges": "bytes",
+//       "Content-Length": "contentLength",
+//       "Content-Type": "video/mp4",
+//   };
+//   res.writeHead(206, headers);
 
-  const videoStream = fs.createReadStream(videoPath, {start, end});
+//   const videoStream = fs.createReadStream(videoPath, {start, end});
 
-  videoStream.pipe(res);
-});
+//   videoStream.pipe(res);
+// });
 
+
+//simple get video from DB
+// app.get('/init-video', (req, res) => {
+//     const db = client.db('videos');
+//     const bucket = new mongoDB.GridFSBucket(db);
+//     const videoPuloadStream = bucket.openDownloadStream('myVid');
+//     const videReadStream =  fs.createReadStream('../myVid.mp4')
+//     videoReadStream.pipe(videoPuloadStream);
+//     res.status(200).send("Done....")
+// });
 
 
