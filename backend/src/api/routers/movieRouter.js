e936e4e40ab88f8a516dd107cf36d2eb7ce13f17
99hash63 <thkottegoda@gmail.com>
2021-07-16 21:56:49 +0530

    Updated movie validations with express-validator

--- backend/src/api/routers/movieRouter.js
+++ backend/src/api/routers/movieRouter.js
@@ -6,32 +6,40 @@ const {check, validationResult} = require('express-validator');
 //@route    POST http://localhost:5000/movies/add
 //@desc     Save new movie to the database
 //@access   private
-router.post("/add", auth, async(req,res)=>{
-    try{
-        const {movieID, name, category, desc, runtime} = req.body;
+router.post("/add", [
+        // @validations
+        check('movieID', 'movieID is empty').not().isEmpty().trim().escape(),
+        check('name', 'name is empty').not().isEmpty().trim().escape(),
+        check('category', 'category is empty').not().isEmpty().trim().escape(),
+        check('desc', 'desc is empty').not().isEmpty().trim().escape(),
+        check('runtime', 'runtime is empty').not().isEmpty().trim().escape()
+    ], 
+    auth, async(req,res)=>{
+        try{
+            const {movieID, name, category, desc, runtime} = req.body;
+            
+            //handling request validations
+            const error = validationResult(req);
+            if(!error.isEmpty())
+                return res.status(400).json({
+                    erroMessage: error
+                });
 
-        //@validations
-        // validating required fields
-        if(!movieID || !name || !category || !desc || !runtime)
-            return res.status(400).json({
-                erroMessage: "Please enter all required fields."
-            });
-        
-        //Checking if movieID already exists
-        const existindID = await Movie.findOne({movieID: movieID})
-        if(existindID)
-            return res.status(400).json({
-                erroMessage: "Movie with same ID already exists"
-            });
+            //Checking if movieID already exists
+            const existindID = await Movie.findOne({movieID: movieID})
+            if(existindID)
+                return res.status(400).json({
+                    erroMessage: "Movie with same ID already exists"
+                });
 
 
-        const newMovie = new Movie({movieID, name, category, desc, runtime})
-        await newMovie.save()
-        res.json("Movie Added");
-    }catch(err){
-        console.error(err);
-        res.status(500).send({status: "Error with adding Movie", error: err.message});
-    }
+            const newMovie = new Movie({movieID, name, category, desc, runtime})
+            await newMovie.save()
+            res.json("Movie Added");
+        }catch(err){
+            console.error(err);
+            res.status(500).send({status: "Error with adding Movie", error: err.message});
+        }
 });
 
 
@@ -73,23 +81,38 @@ router.get('/get/:id', async(req, res) => {
 //@route    PUT http://localhost:5000/movies/update/:id
 //@desc     Update movie with a perticular ID
 //@access   private
-router.put("/update/:id", auth, async(req, res) =>{        
-   
-    try{
-        const {movieID, name, category, desc, runtime} = req.body;
+router.put("/update/:id", [
+        // @validations
+        check('movieID', 'movieID is empty').not().isEmpty().trim().escape(),
+        check('name', 'name is empty').not().isEmpty().trim().escape(),
+        check('category', 'category is empty').not().isEmpty().trim().escape(),
+        check('desc', 'desc is empty').not().isEmpty().trim().escape(),
+        check('runtime', 'runtime is empty').not().isEmpty().trim().escape()
+    ],
+    auth, async(req, res) =>{        
+    
+        try{
+            const {movieID, name, category, desc, runtime} = req.body;
 
-        const updateMovie = {movieID, name, category, desc, runtime}
-        let id = req.params.id;
-        const updatedMovie = await Movie.findByIdAndUpdate(id , updateMovie)
-        if(!updatedMovie)
-            return res.status(400).json({
-                erroMessage: "invalid id"
-            });
-        res.json({status: "Movie Updated"});
-    }catch(err){
-        console.log(err);
-        res.status(500).send({status: "Error with updating movie", error: err.message});
-    }
+            //handling request validations
+            const error = validationResult(req);
+            if(!error.isEmpty())
+                return res.status(400).json({
+                    erroMessage: error
+                });
+
+            const updateMovie = {movieID, name, category, desc, runtime}
+            let id = req.params.id;
+            const updatedMovie = await Movie.findByIdAndUpdate(id , updateMovie)
+            if(!updatedMovie)
+                return res.status(400).json({
+                    erroMessage: "invalid id"
+                });
+            res.json({status: "Movie Updated"});
+        }catch(err){
+            console.log(err);
+            res.status(500).send({status: "Error with updating movie", error: err.message});
+        }
 });
 
 //@route    DELETE http://localhost:5000/movies/delete/:id
