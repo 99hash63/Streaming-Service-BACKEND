515c83fdffcd0b3ea173f6cec89f6fa0ea632fcd
99hash63 <thkottegoda@gmail.com>
2021-07-16 11:26:10 +0530

    created category model and router

--- backend/src/api/routers/categoryRouter.js
+++ backend/src/api/routers/categoryRouter.js
@@ -21,67 +21,71 @@ router.post("/add", auth, async(req,res)=>{
 });
 
 
-//@route    GET http://localhost:5000/movies/get
-//@desc     Get all movies from the database
+
+//@route    GET http://localhost:5000/categories/get
+//@desc     Get all categories from the database
 //@access   public
 router.get("/get", auth, async(req,res)=>{
-    await Movie.find()
-    .then((MovieRequsets)=>{res.json(MovieRequsets);})
-    .catch((err)=>{
+    try{
+        const CategoryRequsets = await Category.find()
+        res.json(CategoryRequsets);
+    }catch(err){
         console.log(err);
-        res.status(500).send({status: "Error with getting movies", error: err.message});
-    })
+        res.status(500).send({status: "Error with getting categories", error: err.message});
+    }
 })
 
 
 
-//@route    GET http://localhost:5000/movies/get/:id
-//@desc     Get movie for a perticular ID
+//@route    GET http://localhost:5000/categories/get/:id
+//@desc     Get category for a perticular ID
 //@access   public
 router.get('/get/:id', auth, async(req, res) => {
+    try{
+        let id = req.params.id;
 
-    let id = req.params.id;
-
-    await Movie.findById(id)
-    .then(Movie => res.json(Movie))
-    .catch((err)=>{
+        const CategoryRequset = await Category.findById(id)
+        res.json(CategoryRequset);
+    }catch(err){
         console.log(err);
-        res.status(500).send({status: "Error with getting movie", error: err.message});
-    });
+        res.status(500).send({status: "Error with getting category", error: err.message});
+    }
 });
    
 
-//@route    PUT http://localhost:5000/movies/update/:id
-//@desc     Update movie with a perticular ID
+//@route    PUT http://localhost:5000/categories/update/:id
+//@desc     Update category with a perticular ID
 //@access   public
 router.put("/update/:id", auth, async(req, res) =>{        
    
-    const {movieID, name, category, desc, runtime} = req.body;
+    try{
+        const {categoryID, name, desc} = req.body;
 
-    const updateMovie = {movieID, name, category, desc, runtime}
-    let id = req.params.id;
-    const update = await Movie.findByIdAndUpdate(id , updateMovie)
-    .then(()=>{ res.json({status: "Movie Updated"})})
-    .catch((err)=>{
+        const updateCategory = {categoryID, name, desc}
+        let id = req.params.id;
+        const update = await Category.findByIdAndUpdate(id , updateCategory)
+        res.json({status: "Category Updated"});
+    }catch(err){
         console.log(err);
-        res.status(500).send({status: "Error with updating movie", error: err.message});
-    });
+        res.status(500).send({status: "Error with updating category", error: err.message});
+    }
 });
 
-//@route    DELETE http://localhost:5000/movies/delete/:id
-//@desc     Dlete movie wit a perticular ID
+//@route    DELETE http://localhost:5000/categories/delete/:id
+//@desc     Dlete category with a perticular ID
 //@access   public
 router.delete("/delete/:id", auth, async(req,res)=>{
+    try{
+        let Id = req.params.id;
 
-    let Id = req.params.id;
-
-    await Movie.findByIdAndDelete(Id)
-    .then(()=>{res.status(200).send({status: "Movie deleted"});})
-    .catch((err)=>{
+        await Category.findByIdAndDelete(Id)
+        res.status(200).send({status: "Category deleted"});
+    }catch(err){
         console.log(err.message);
-        res.status(500).send({status: "error with deleting movie", error: err.message});
-    });
+        res.status(500).send({status: "error with deleting category", error: err.message});
+    }
 });
+
 
 module.exports = router;
 
